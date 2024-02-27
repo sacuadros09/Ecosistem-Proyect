508b292c69792f01d21a08c45d174dcb408e6d27
sacuadros09 <sacuma09@gmail.com>
2024-02-27 18:58:55 -0500

    Server

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const z = require('zod');
+const express  = require("express")
+const cors = require ("cors")
+const z = require("zod")
 
-const app = express();
-app.use(cors());
-app.use(express.json());
 
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+
+app.listen(3000,() => {
+    console.log("Server is running on port 3000")
+})
