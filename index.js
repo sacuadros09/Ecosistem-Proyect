d137af81b71d55e31bd7582c03ab912d1ca4326b
sacuadros09 <sacuma09@gmail.com>
2024-02-27 21:03:36 -0500

    z

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/', (req,res) => {
 app.get('/users', (req,res) => {
    console.log("Query:",req.query)
    console.log("Headers:",req.headers)
-    res.send({id})
+    res.send({users})
 })
 
 app.get('/users/:id', (req,res) => {
@@ -25,6 +25,43 @@ app.get('/users/:id', (req,res) => {
     res.send({id})
 })
 
+app.post('/users', (req, res) => {    
+    const {name, last, email,code,password} = req.body;
+    if (!name || !last || !email || !code || !password) {
+        return res.status(400).send({error: "Invalid data"});
+    }
+    const user = {
+        name,
+        last,
+        email,
+        code,
+        password
+    };
+    users.push(user);
+    res.send({user});
+});
+
+app.post('/tasks', (req, res) => {
+const taskSchema = z.object({
+    name: z.string(),
+    last: z.string(),
+    email: z.string().email(),
+    code: z.union([z.string(), z.number()]),
+    password: z.string()
+});
+const taskValidation = taskSchema.safeParse(req.body);
+
+if (!taskValidation.success) {
+    return res.status(400).send({ error: "Invalid data", details: taskValidation.error });
+}
+
+
+const sanitizedTask = { ...taskValidation.data };
+
+
+sanitizedTask.password = "********";
+res.send({ task: sanitizedTask });
+});
 
 app.listen(3000,() => {
     console.log("Server is running on port 3000")
